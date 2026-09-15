d6e408fc750c7f4f4c50c61da8ac8bb68ffaf1be
bhimsenrao <brkaran2k2@gmail.com>
2026-09-15 12:09:51 +0530

    workflows updated version 3 vite.config.js changed

--- app-crud/vite.config.js
+++ app-crud/vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+// import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react()],
+  base: '/Crud/',  // Change to your repo name
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true
+      }
+    }
+  }
 })
